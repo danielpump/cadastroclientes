26fc81806afae0a60e4e612f222b0fad524746ac
Daniel Ferraz <new_daniel2010@hotmail.com>
2017-08-04 21:17:05 -0300

    Pacote inicial e classe inicializadora da aplicação.

---
+++ src/main/java/com/semnome/Application.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.semnome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Daniel
+ *
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
