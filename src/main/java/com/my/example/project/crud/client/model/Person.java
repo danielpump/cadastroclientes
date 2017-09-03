0ef84e98acd62e61fe5e6a18a6d9b7643b610717
Daniel Ferraz <new_daniel2010@hotmail.com>
2017-09-03 13:53:51 -0300

    Salvando e atualizando lista de veículos de pessoa

--- src/main/java/com/my/example/project/crud/client/model/Person.java
+++ src/main/java/com/my/example/project/crud/client/model/Person.java
@@ -3,14 +3,18 @@ package com.my.example.project.crud.client.model;
 import java.util.Collections;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.Cascade;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
@@ -42,7 +46,7 @@ public abstract class Person {
 	
 	private String document;
 	
-	@OneToMany
+	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
 	private List<Car> cars;
 
 	public Long getId() {
